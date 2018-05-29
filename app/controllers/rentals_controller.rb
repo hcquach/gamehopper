152f94a77be9fb103cc90cce6b529f7a52463cf6
oussama ladhari <oussama.ladhari@gmail.com>
2018-05-29 12:19:36 +0900

    deleted the edit and update from before action in rental controllers

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,5 +1,5 @@
 class RentalsController < ApplicationController
-   before_action :set_rental, only: [:show, :edit, :update, :destroy]
+   before_action :set_rental, only: [:show, :destroy]
   def index
     @rentals = Rental.all
   end
