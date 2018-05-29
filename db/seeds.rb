78cea76fac717bb3ae40b333ee8432bacf1fca60
Hien-Chanh Quach <hcquach@gmail.com>
2018-05-29 17:35:46 +0900

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Game.destroy_all if Rails.env.development?
-
-Game.create(title: "Zelda", description: "The Legend of Zelda: Breath of the Wild is an action-adventure game developed and published by Nintendo.", platform: "SWITCH", photo: "image/upload/v1527504348/st5fzyj3rdwtazduuu5r.jpg", user_id: 1)
-puts
-puts "Game Added completed"
-
-Game.create(title: "Mario Kart 8 Deluxe", description: "Hit the road with the definitive version of Mario Kart 8 and play anytime, anywhere! Race your friends or battle them in a revised battle mode on new and returning battle courses.", platform: "SWITCH", photo: "image/upload/v1527573901/cttchmkihhunnt6xltro.jpg", user_id: 1)
-
-puts
-puts "Game Added completed"
-
-User.destroy_all if Rails.env.development?
+# User.destroy_all if Rails.env.development?
 
 User.create(username: "oladhari", email: "oussama@gmail.com", password:  "topsecret", password_confirmation: "topsecret", address: "Nara, Japan")
 puts
@@ -34,3 +23,19 @@ puts "User Added completed"
 User.create(username: "gianluca24", email: "luca@gmail.com", password:  "topsecret", password_confirmation: "topsecret", address: "Manila, Philippines")
 puts
 puts "User Added completed"
+
+# Game.destroy_all if Rails.env.development?
+
+url_one = "http://res.cloudinary.com/le-wagon-hcquach/image/upload/v1527582718/download.jpg"
+game_one = Game.create(title: "Zelda", description: "The Legend of Zelda: Breath of the Wild is an action-adventure game developed and published by Nintendo.", platform: "SWITCH", photo: "v1527504348/st5fzyj3rdwtazduuu5r.jpg", user_id: 1)
+game_one.remote_photo_url = url_one
+game_one.save
+puts
+puts "Game Added completed"
+
+url_two = "http://res.cloudinary.com/le-wagon-hcquach/image/upload/v1527573901/cttchmkihhunnt6xltro.jpg"
+game_two = Game.create(title: "Mario Kart 8 Deluxe", description: "Hit the road with the definitive version of Mario Kart 8 and play anytime, anywhere! Race your friends or battle them in a revised battle mode on new and returning battle courses.", platform: "SWITCH", photo: "ilgamjlury6gekkghwci.jpg", user_id: 1)
+game_two.remote_photo_url = url_two
+game_two.save
+puts
+puts "Game Added completed"
