04067854c66071cce484963d474d1b7ed26cd2af
Hien-Chanh Quach <hcquach@gmail.com>
2018-05-31 15:57:45 +0900

    Changed the game show and added modal and conditional for game deletion and rental

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -41,8 +41,10 @@ class GamesController < ApplicationController
   end
 
   def destroy
-    @game.destroy
-    redirect_to games_path
+    if @game.user == current_user
+      @game.destroy
+      redirect_to games_path
+    end
   end
 
   private
