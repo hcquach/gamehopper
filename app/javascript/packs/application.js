b9e0b457f4ac2ee3a09b2d8875527e95a65ffbe5
Hien-Chanh Quach <hcquach@gmail.com>
2018-06-01 09:38:26 +0900

    Fixed modal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,16 @@
 import "bootstrap";
+
+var globalModal = $('.global-modal');
+    $( ".btn-green-flat-trigger" ).on( "click", function(e) {
+      e.preventDefault();
+      $( globalModal ).toggleClass('global-modal-show');
+    });
+    $( ".overlay" ).on( "click", function() {
+      $( globalModal ).toggleClass('global-modal-show');
+    });
+    $( ".global-modal_close" ).on( "click", function() {
+      $( globalModal ).toggleClass('global-modal-show');
+    });
+    $(".mobile-close").on("click", function(){
+      $( globalModal ).toggleClass('global-modal-show');
+    });
